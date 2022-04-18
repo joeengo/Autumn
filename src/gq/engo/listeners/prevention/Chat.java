4bb24b560b4058ed9edb58c4288d5b61a449cca4
engo <63791277+joeengo@users.noreply.github.com>
2022-04-18 16:50:45 +0100

    anti advertise

--- src/gq/engo/listeners/prevention/Chat.java
+++ src/gq/engo/listeners/prevention/Chat.java
@@ -2,49 +2,39 @@ package gq.engo.listeners.prevention;
 
 import gq.engo.Plugin;
 import gq.engo.utils.C;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
-public class Chat implements Listener {
-    public final java.util.HashMap<Player, Integer> chats = new java.util.HashMap<>();
+import java.util.List;
 
-    /*
+public class Chat implements Listener {
+    List<String> blacklist = Plugin.Instance.getConfig().getStringList("Chat.Blacklist");
+    List<String> whitelist = Plugin.Instance.getConfig().getStringList("Chat.Whitelist");
+    boolean enabled = Plugin.Instance.getConfig().getBoolean("Chat.Enabled");
+
+    public boolean isBlacklisted(String s) {
+        for (String i : blacklist) {
+            if (s.toLowerCase().contains(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     @EventHandler
     public void AsyncPlayerChatEvent(AsyncPlayerChatEvent e) {
         Player p = e.getPlayer();
-        int limit = Plugin.Instance.getConfig().getInt("Chat.MaxPerSecond");
-        boolean enabled = Plugin.Instance.getConfig().getBoolean("Chat.Enabled");
-        if(enabled == false) { return; }
-        if (chats.containsKey(e.getPlayer())) {
-            chats.put(e.getPlayer(), chats.get(e.getPlayer()) + 1);
-        } else {
-            chats.put(e.getPlayer(), 1);
+        if (!enabled) return;
+        String checkmesssage = e.getMessage();
+        for (String i : whitelist) {
+            checkmesssage = checkmesssage.replaceAll(i, "");
         }
-        new java.util.Timer().schedule(
-                new java.util.TimerTask() {
-                    @Override
-                    public void run() {
-                        chats.put(e.getPlayer(), chats.get(e.getPlayer())-1);
-                    }
-                },
-                1000
-        );
-        if (chats.get(e.getPlayer()) > limit) {
+
+        if (isBlacklisted(checkmesssage)) {
+            e.getPlayer().sendMessage("<" + e.getPlayer().getName() + "> " + (e.getMessage().replaceAll(">", "")));
             e.setCancelled(true);
-            e.getPlayer().sendMessage(C.addPrefix(C.getThird("You are chatting too fast! Chat Cooldown: " + (1/limit) + "s")));
-            if (chats.get(e.getPlayer()) > limit+3 && Plugin.Instance.getConfig().getBoolean("Chat.Kick")) {
-                Bukkit.getScheduler().runTask(Plugin.Instance, new Runnable() {
-                    public void run() {
-                        e.getPlayer().kickPlayer(C.addPrefix(C.getThird("You are chatting too fast! Chat Cooldown: " + (1/limit) + "s")));
-                    }
-                });
-            }
         }
     }
-    */
 }
